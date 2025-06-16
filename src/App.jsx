f56b09d47ce2b251291f2baa621a0a82df2e8ea8
Mostazzita <mostazzita@proton.me>
2025-06-16 13:32:27 -0400

    capturar valor del campo

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,19 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    
+    console.log(location)
+  }
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value)
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} >
         <label htmlFor="locationInput">
           Ingrese una ubicación
-          <input type="text" id="locationInput" value={location}/>
+          <input onChange={handleInputChange} value={location} type="text" id="locationInput" />
         </label>
         <button type="submit">Buscar</button>
       </form>
